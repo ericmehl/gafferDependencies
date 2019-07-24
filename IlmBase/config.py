dbab84ed99107b2bd5de1350c45609726cd5d282
Eric Mehl <eric@thinkhypothetical.com>
2019-07-24 15:09:05 -0400

    OpenEXR : add Windows configuration

--- IlmBase/config.py
+++ IlmBase/config.py
@@ -2,7 +2,7 @@
 
 	"downloads" : [
 
-		"https://github.com/openexr/openexr/releases/download/v2.3.0/ilmbase-2.3.0.tar.gz"
+		"https://github.com/openexr/openexr/releases/download/v2.3.0/ilmbase-2.3.0.zip"
 
 	],
 
@@ -20,15 +20,7 @@
 
 		"commands" : [
 
-			"mkdir gafferBuild",
-			"cd gafferBuild && "
-				" cmake"
-				" -Wno-dev"
-				" -G {cmakeGenerator}"
-				" -D OPENEXR_BUILD_SHARED=ON"	# this will change to BUILD_SHARED_LIBS=ON post 2.3.0
-				" -D CMAKE_INSTALL_PREFIX={buildDir}"
-				" ..",
-			"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install -j {jobs}",
+			"IlmBase on Windows is built as part of OpenEXR",
 		]
 	},
 
--- OpenEXR/config.py
+++ OpenEXR/config.py
@@ -16,4 +16,34 @@
 
 	],
 
+	"platform:windows" : {
+
+		"downloads" : [
+
+			"https://github.com/openexr/openexr/archive/v2.3.0.zip"
+
+		],
+
+
+		"commands" : [
+			"mkdir gafferBuild",
+			"cd gafferBuild && cmake"
+				" -DCMAKE_INSTALL_PREFIX={buildDir}"
+				" .."
+				" -G {cmakeGenerator}"
+				# " -D OPENEXR_BUILD_ILMBASE=OFF"
+				# " -D OPENEXR_BUILD_OPENEXR=ON"
+				# " -D OPENEXR_BUILD_PYTHON_LIBS=ON"
+				# " -D OPENEXR_BUILD_VIEWERS=OFF"
+				" -DCMAKE_PREFIX_PATH={buildDir}"
+				# " -D BOOST_ROOT={buildDir}"
+				# " -D Boost_INCLUDE_DIRS={buildDir}\\include\\boost-1_61"
+				# " -D PYTHON_INCLUDE_PATH={buildDir}\\include"
+				# " -D ILMBASE_PACKAGE_PREFIX={buildDir}"
+				# " -D ILMBASE_LOCATION={buildDir}"
+				" ..",
+			"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install -j {jobs}",
+		]
+	},
+
 }
