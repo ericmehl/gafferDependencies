30f58672b88b89ba58029166b8ae9237eb535430
Eric Mehl <eric@thinkhypothetical.com>
2019-07-24 17:56:33 -0400

    OpenVDB : add Windows configuration

--- OpenVDB/config.py
+++ OpenVDB/config.py
@@ -50,9 +50,48 @@
 		"include/openvdb",
 		"include/pyopenvdb.h",
 		"include/nanovdb",
-		"lib/libopenvdb*{sharedLibraryExtension}*",
+		"lib/{libraryPrefix}openvdb*{sharedLibraryExtension}*",
+		"lib/{libraryPrefix}openvdb*.lib",
 		"python/pyopenvdb*",
 
 	],
+
+	"platform:windows" : {
+
+		"variables" : {
+		
+			"cmakeGenerator" : "\"Visual Studio 15 2017 Win64\"",
+		},
+
+		"commands" : [
+			# OpenVDB requests Python 2.7 specifically but Boost doesn't add version numbers until v1.67
+			"mkdir gafferBuild",
+			"cd gafferBuild && "
+				" cmake"
+				" -Wno-dev"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" -D OPENVDB_BUILD_UNITTESTS=OFF"
+				" -D OPENVDB_BUILD_DOCS=OFF"
+				" -D OPENVDB_BUILD_PYTHON_MODULE=ON"
+				" -D OPENVDB_BUILD_NANOVDB=ON"
+				" -D USE_GLFW3=OFF"
+				" -D TBB_LOCATION={buildDir}"
+				" -D BOOST_ROOT={buildDir}"
+				" -D GLEW_LOCATION={buildDir}"
+				" -D ILMBASE_LOCATION={buildDir}"
+				" -D OPENEXR_LOCATION={buildDir}"
+				" -D BLOSC_LOCATION={buildDir}"
+				" -D OPENVDB_ENABLE_3_ABI_COMPATIBLE=OFF"
+				" ..",
+
+			"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install",
+			"move {buildDir}\\bin\\{libraryPrefix}openvdb*{sharedLibraryExtension}* {buildDir}\\lib\\",
+			"move {buildDir}\\lib\\python{pythonVersion}\\site-packages\\pyopenvdb.pyd {buildDir}\\python\\pyopenvdb.pyd",
+			"move {buildDir}\\lib\\python{pythonVersion}\\site-packages\\pyopenvdb*.lib {buildDir}\\lib\\"
+		],
+
+	},
 
 }
