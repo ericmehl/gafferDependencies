e444b091523d1ed15fab2a80eb2588db12d4b738
Eric Mehl <eric@thinkhypothetical.com>
2022-05-13 17:44:54 -0400

    Cycles : Add Windows configuration

--- Cycles/config.py
+++ Cycles/config.py
@@ -43,4 +43,36 @@
 
 	],
 
+	"platform:windows" : {
+
+		"commands" : [
+
+			"mkdir build",
+			"cd build &&"
+				" cmake"
+				" -W-nodev -G {cmakeGenerator}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}/cycles"
+				" -D CMAKE_PREFIX_PATH={buildDir}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D WITH_CYCLES_OPENIMAGEDENOISE=OFF"
+				" -D WITH_CYCLES_DEVICE_CUDA=OFF"
+				" -D WITH_CYCLES_DEVICE_OPTIX=OFF"
+				" -D CMAKE_POSITION_INDEPENDENT_CODE=ON"
+				" -D TBB_ROOT_DIR={buildDirFwd}"
+				" -D OPENVDB_ROOT_DIR={buildDirFwd}"
+				" -D NANOVDB_INCLUDE_DIR={buildDirFwd}/include"
+				" -D GLEW_ROOT_DIR={buildDirFwd}"
+				" ..",
+			"set PATH={buildDir}/lib;%PATH% && cd build && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}",
+			"if not exist \"{buildDir}\\cycles\\bin\" mkdir {buildDir}\\cycles\\bin",
+			"if not exist \"{buildDir}\\cycles\\include\" mkdir {buildDir}\\cycles\\include",
+			"copy build\\bin\\cycles.* {buildDir}\\cycles\\bin",
+			"xcopy /sehyi build\\lib {buildDir}\\cycles\\lib",
+			"xcopy /sehyi third_party\\atomic\\* {buildDir}\\cycles\\include",
+			"xcopy /shyi src\\*.h K:\\build\\gaffer\\cycles\\include",
+
+		]
+
+	}
+
 }
