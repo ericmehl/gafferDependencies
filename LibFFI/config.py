159d6e13e2e2e6b542d1973a0fcc005227147705
Eric Mehl <eric@thinkhypothetical.com>
2026-01-12 10:15:15 -0500

    LIbFFI : Windows config

--- LibFFI/config.py
+++ LibFFI/config.py
@@ -11,9 +11,9 @@
 
 	"commands" : [
 
-		"./configure --prefix={buildDir} --libdir={buildDir}/lib --disable-multi-os-directory --without-gcc-arch",
+		"sh configure {args} -prefix={buildDir} --libdir={buildDir}/lib",
 		"make -j {jobs}",
-		"make install",
+		"{installCommand}",
 
 	],
 
@@ -22,5 +22,31 @@
 		"lib/libffi*{sharedLibraryExtension}*",
 
 	],
+
+	"variables" : {
+
+		"args" : "--disable-multi-os-directory --without-gcc-arch",
+		"installCommand" : "make install",
+
+	},
+
+	"platform:windows" : {
+
+		"variables" : {
+
+			"host" : "x86_64-w64-cygwin",
+			"args" : "CC='../msvcc.sh -m64' CXX='../msvcc.sh -m64' LD='link' CPP='cl -nologo -EP' CXXCPP='cl -nologo -EP' CPPFLAGS='-DFFI_BUILDING_DLL' AR='../.travis/ar-lib lib' NM='dumpbin -symbols' STRIP=':' --build={host} --host={host} --disable-static --enable-shared",
+			"installCommand" : "",
+
+		},
+
+		"postBuildCopy" : [
+
+			( "{host}/include", "*.h", "{buildDir}/include" ),
+			( "{host}/.libs", "libffi-8.*", "{buildDir}/lib" ),
+
+		]
+
+	},
 
 }
