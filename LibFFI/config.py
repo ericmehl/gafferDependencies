e9f008ab5d443d1119d37406f35b64ecb6737a08
Eric Mehl <eric@thinkhypothetical.com>
2026-01-12 10:15:15 -0500

    LIbFFI : Window config

--- LibFFI/config.py
+++ LibFFI/config.py
@@ -11,7 +11,7 @@
 
 	"commands" : [
 
-		"./configure --prefix={buildDir} --libdir={buildDir}/lib --disable-multi-os-directory --without-gcc-arch",
+		"sh configure {preArgs} --prefix={buildDir} --libdir={buildDir}/lib {postArgs}",
 		"make -j {jobs}",
 		"make install",
 
@@ -22,5 +22,23 @@
 		"lib/libffi*{sharedLibraryExtension}*",
 
 	],
+
+	"variables" : {
+
+		"preArgs" : "",
+		"postArgs" : "--disable-multi-os-directory --without-gcc-arch",
+
+	},
+
+	"platform:windows" : {
+
+		"variables" : {
+
+			"preArgs" : '--enable-shared --build x86_64-w64-mingw32 --host x86_64-w64-mingw32 CC="../msvcc.sh -m64" CXX="../msvcc.sh -m64" LD=link CPP="cl -nologo -EP" CXXCPP="cl -nologo -EP" CPPFLAGS="-DFFI_BUILDING_DLL" AR=\'../.ci/ar-lib lib\' NM=\'dumpbin -symbols\' STRIP=\':\'',
+			"postArgs" : "",
+
+		},
+
+	},
 
 }
